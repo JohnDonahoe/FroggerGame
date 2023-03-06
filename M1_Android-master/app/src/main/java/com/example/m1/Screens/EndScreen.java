add3390e9bc348e6c5d3b5b902aa183aff32b3eb
jdonahoe8 <jdonahoe8@gatech.edu>
2023-03-06 11:29:09 -0500

    Fixed Manifest

--- M1_Android-master/app/src/main/java/com/example/m1/Screens/EndScreen.java
+++ M1_Android-master/app/src/main/java/com/example/m1/Screens/EndScreen.java
@@ -1,3 +1,7 @@
-public class EndScreen {
+package com.example.m1.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EndScreen extends AppCompatActivity {
     
 }
--- M1_Android-master/app/src/main/java/com/example/m1/Screens/GameScreen.java
+++ M1_Android-master/app/src/main/java/com/example/m1/Screens/GameScreen.java
@@ -1,6 +1,7 @@
 package com.example.m1.Screens;
+import androidx.appcompat.app.AppCompatActivity;
 import com.example.m1.Classes.*;
 
-public class GameScreen {
+public class GameScreen extends AppCompatActivity {
 
 }
