8b2859157df8c49184c3eb8b3bdec63a14775200
jdonahoe8 <jdonahoe8@gatech.edu>
2023-03-05 21:54:47 -0500

    Added Start Screen and began config screen

--- M1_Android-master/app/src/main/java/com/example/m1/Screens/ConfigScreen.java
+++ M1_Android-master/app/src/main/java/com/example/m1/Screens/ConfigScreen.java
@@ -1,3 +1,11 @@
-public class ConfigScreen {
+package com.example.m1.Screens;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.m1.R;
+
+public class ConfigScreen extends AppCompatActivity {
     
 }
--- M1_Android-master/app/src/main/java/com/example/m1/Screens/StartScreen.java
+++ M1_Android-master/app/src/main/java/com/example/m1/Screens/StartScreen.java
@@ -1,3 +1,30 @@
-public class StartScreen {
+package com.example.m1.Screens;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.m1.R;
+
+public class StartScreen extends AppCompatActivity {
+    
+    protected void onCreate(Bundle save) {
+        super.onCreate(save);
+
+        setContentView(R.layout.start_screen);
+
+        Button start = findViewById(R.id.startButton);
+        start.setOnClickListener(v -> openConfigScreen());
+
+
+        Button exit = findViewById(R.id.exitButton);
+        exit.setOnClickListener(v -> finish());
+    }
+
+    public void openConfigScreen() {
+        Intent intent = new Intent(getApplicationContext(), ConfigScreen.class);
+        startActivity(intent);
+        setContentView(R.layout.config_screen);
+    }
     
 }
