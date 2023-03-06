161d53244be265d3541c269a5cbd484b2f442263
jdonahoe8 <jdonahoe8@gatech.edu>
2023-03-06 12:22:58 -0500

    Created new bitmap inside GameScreen

--- M1_Android-master/app/src/main/java/com/example/m1/Screens/GameScreen.java
+++ M1_Android-master/app/src/main/java/com/example/m1/Screens/GameScreen.java
@@ -1,5 +1,6 @@
 package com.example.m1.Screens;
 import android.os.Bundle;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.m1.Classes.*;
 import com.example.m1.R;
@@ -12,6 +13,14 @@ public class GameScreen extends AppCompatActivity {
 
     private Canvas canvas;
 
+    private TextView lives;
+
+    private TextView name;
+
+    private TextView score;
+
+    private TextView difficulty;
+
 
 
     protected void onCreate(Bundle init) {
@@ -21,6 +30,14 @@ public class GameScreen extends AppCompatActivity {
     }
 
     private void initializeGame() {
+        lives = (TextView) findViewById(R.id.numLives);
+        name = (TextView) findViewById(R.id.textView5);
+        score = (TextView) findViewById(R.id.textView10);
+        difficulty = (TextView) findViewById(R.id.textView8);
+    }
 
+    private void builtBitmap() {
+        bitmap = Bitmap.createBitmap(1080, 1080, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
     }
 }
